4d442a3ada8b91e853f70bfe746512e10354ea68
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-09-06 15:35:36 +0200

    Add shutdown hook to close environment
    In super stream consumer sample. This way the consumers
    can exit cleanly and store offsets properly when the process
    is killed e.g. with Ctrl-C.

--- src/main/java/com/rabbitmq/stream/SuperStreamConsumer.java
+++ src/main/java/com/rabbitmq/stream/SuperStreamConsumer.java
@@ -10,36 +10,23 @@ public class SuperStreamConsumer {
       System.exit(1);
     }
     String instanceName = args[0];
-    try (Environment environment =
-        Environment.builder()
-            .maxConsumersByConnection(1)
-            .maxTrackingConsumersByConnection(1)
-            .build()) {
+    try (Environment environment = Environment.builder().maxConsumersByConnection(1)
+        .maxTrackingConsumersByConnection(1).build()) {
+
+      // to exit properly when Ctrl-C-ed
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> environment.close()));
 
       String superStream = "invoices";
       String reference = "my-app";
 
       System.out.println("Starting consumer " + instanceName);
-      environment
-          .consumerBuilder()
-          .superStream(superStream)
-          .name(reference)
-          .singleActiveConsumer()
-          .autoTrackingStrategy()
-          .messageCountBeforeStorage(10)
-          .builder()
-          .messageHandler(
-              (context, message) -> {
-                System.out.println(
-                    "Consumer "
-                        + instanceName
-                        + " received message "
-                        + message.getProperties().getMessageId()
-                        + " from stream "
-                        + context.stream()
-                        + ".");
-              })
-          .build();
+      environment.consumerBuilder().superStream(superStream).name(reference).singleActiveConsumer()
+          .autoTrackingStrategy().messageCountBeforeStorage(10).builder()
+          .messageHandler((context, message) -> {
+            System.out.println(
+                "Consumer " + instanceName + " received message " + message.getProperties()
+                    .getMessageId() + " from stream " + context.stream() + ".");
+          }).build();
       Scanner keyboard = new Scanner(System.in);
       keyboard.nextLine();
     }
